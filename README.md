2c535bd832d727f8118554fe99e82fa4fa6738a0
dorathip <dorathiparanjothi@gmail.com>
2024-10-25 12:57:38 -0400

    Created an mental health app using ReactJS and Node.js

--- README.md
+++ README.md
@@ -1,70 +1,57 @@
-# Getting Started with Create React App
+To create an application in Node.js and React that presents a questionnaire and captures the data in a SQLite database, we need to follow these steps:
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Step 1: Initialize Node.js Project
 
-## Available Scripts
+### `mkdir mental-health-app`
 
-In the project directory, you can run:
+### `cd mental-health-app`
+
+### `npm init -y`
+
+### `npm install express sqlite3 body-parser cors`
+
+# Step 2: Set Up Express Server : Create a file named server.js and implement the backend logic on this file
+
+# Step 3: Initialize React Project
+
+### `npx create-react-app mental-health-client`
+
+### `cd mental-health-client`
+
+### `npm install axios`
+
+# Step 4: Create Questionnaire Form : Modify src/App.js to create the form and handle submissions and modify a file named App.css in the src directory of your React project according to your style.
+
+# Step 5: Running the Application
+
+# 1. Run the Backend:
+
+The node_modules folder is ignored when converting to a Zip file because the node_modules folder can be extremely large, containing thousands of files and directories. We can easily recreate the node_modules directory by installing the dependencies listed in the package.json file. Here are the steps:
+
+### `cd mental-health-app`
+
+### `npm install`
+
+Now run the backend server:
+
+### `node server.js`
+
+The node_modules folder is ignored when converting to a Zip file because the node_modules folder can be extremely large, containing thousands of files and directories. We can easily recreate the node_modules directory by installing the dependencies listed in the package.json file. Here are the steps:
+
+# 2. Install Dependencies: Use a package manager like npm to install the dependencies. This will recreate the node_modules folder based on the package.json and package-lock.json files.
+
+### `cd mental-health-client`
+
+### `npm install`
+
+This command will read the package.json file, download all the required dependencies, and place them in a newly created node_modules folder. After this, we can run the forntend of the project as intended.
+
+# 3. Run the Frontend:
 
 ### `npm start`
 
 Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Open [http://localhost:3000](http://localhost:3000) to view it in our browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+The page will reload when we make changes.\
+We may also see any lint errors in the console.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,199 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const initialFormData = {
+    full_name: '',
+    date_of_birth: '',
+    happiness: '',
+    energy: '',
+    hopefulness: '',
+    sleep_hours: ''
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [statistics, setStatistics] = useState(null);
+  const [individualStats, setIndividualStats] = useState(null);
+  const [ageGroupStatistics, setAgeGroupStatistics] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSubmitting) {
+      return;
+    }
+
+    setIsSubmitting(true);
+    axios.post('http://localhost:5000/submit', formData)
+      .then(response => {
+        alert('Submission successful');
+        fetchStatistics();
+        fetchIndividualStatistics();
+        fetchAgeGroupStatistics();
+        setFormData(initialFormData); // Clear the form after submission
+        setIsSubmitting(false);
+      })
+      .catch(error => {
+        console.error('There was an error submitting the form!', error);
+        setIsSubmitting(false);
+      });
+  };
+
+  const fetchStatistics = () => {
+    axios.get('http://localhost:5000/statistics')
+      .then(response => {
+        setStatistics(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the statistics!', error);
+      });
+  };
+
+  const fetchIndividualStatistics = () => {
+    axios.post('http://localhost:5000/individual-statistics', {
+      full_name: formData.full_name,
+      date_of_birth: formData.date_of_birth
+    })
+      .then(response => {
+        setIndividualStats(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the individual statistics!', error);
+      });
+  };
+
+  const fetchAgeGroupStatistics = () => {
+    axios.get('http://localhost:5000/age-group-statistics')
+      .then(response => {
+        setAgeGroupStatistics(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the age group statistics!', error);
+      });
+  };
+
+  useEffect(() => {
+    // Initial fetch for statistics if needed
+    fetchStatistics();
+    fetchAgeGroupStatistics();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mental Health Questionnaire</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your full name:
+          <input type="text" name="full_name" value={formData.full_name} onChange={handleChange} required />
+        </label>
+        <label>
+          Enter your date of birth (mm/dd/yyyy):
+          <input type="date" name="date_of_birth" value={formData.date_of_birth} onChange={handleChange} required />
+        </label>
+        <label>
+          On a scale from 1-5, how happy do you feel?
+          <input type="number" name="happiness" value={formData.happiness} onChange={handleChange} min="1" max="5" required />
+        </label>
+        <label>
+          On a scale from 1-5, how energetic do you feel?
+          <input type="number" name="energy" value={formData.energy} onChange={handleChange} min="1" max="5" required />
+        </label>
+        <label>
+          On a scale from 1-5, how hopeful do you feel about the future?
+          <input type="number" name="hopefulness" value={formData.hopefulness} onChange={handleChange} min="1" max="5" required />
+        </label>
+        <label>
+          How many hours have you slept last night?
+          <input type="number" name="sleep_hours" value={formData.sleep_hours} onChange={handleChange} required />
+        </label>
+        <button type="submit" disabled={isSubmitting}>Submit</button>
+      </form>
+
+      {statistics && (
+        <div className="statistics">
+          <h2>Statistics by Age Year</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Date of Birth</th>
+                <th>Average Happiness</th>
+                <th>Average Energy</th>
+                <th>Average Hopefulness</th>
+                <th>Average Sleep Hours</th>
+              </tr>
+            </thead>
+            <tbody>
+              {statistics.map((stat, index) => (
+                <tr key={index}>
+                  <td>{stat.date_of_birth}</td>
+                  <td>{stat.avg_happiness}</td>
+                  <td>{stat.avg_energy}</td>
+                  <td>{stat.avg_hopefulness}</td>
+                  <td>{stat.avg_sleep_hours}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {individualStats && (
+        <div className="individual-statistics">
+          <h2>Individual Statistics</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Average Happiness</th>
+                <th>Average Energy</th>
+                <th>Average Hopefulness</th>
+                <th>Average Sleep Hours</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{individualStats.avg_happiness}</td>
+                <td>{individualStats.avg_energy}</td>
+                <td>{individualStats.avg_hopefulness}</td>
+                <td>{individualStats.avg_sleep_hours}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {ageGroupStatistics && (
+        <div className="age-group-statistics">
+          <h2>Statistics by Age Group</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Age Group</th>
+                <th>Average Happiness</th>
+                <th>Average Energy</th>
+                <th>Average Hopefulness</th>
+                <th>Average Sleep Hours</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ageGroupStatistics.map((stat, index) => (
+                <tr key={index}>
+                  <td>{stat.age_group}</td>
+                  <td>{stat.avg_happiness}</td>
+                  <td>{stat.avg_energy}</td>
+                  <td>{stat.avg_hopefulness}</td>
+                  <td>{stat.avg_sleep_hours}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
